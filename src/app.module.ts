f7b64ba9d05e22792d76d9a1a49c171da2048eab
Yash Savani <yashsavani@Yashs-MacBook-Pro.local>
2023-05-17 22:22:00 +0530

    backend folder donw

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { CommentsModule } from './comments/comments.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { VideoModule } from './video/video.module';
+import { LibraryModule } from './library/library.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { SuggestionsModule } from './suggestions/suggestions.module';
 
 @Module({
   imports: [
@@ -12,6 +16,10 @@ import { ConfigModule } from '@nestjs/config';
     CommentsModule,
     AuthModule,
     PrismaModule,
+    VideoModule,
+    LibraryModule,
+    SubscriptionModule,
+    SuggestionsModule,
   ],
   controllers: [],
   providers: [],
