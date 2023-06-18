55e0646cf7b4867f45ab3de79043a8fba871ca68
Yash Savani <yashsavani@Yashs-MacBook-Pro.local>
2023-06-18 22:38:43 +0530

    comment done

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,20 +1,35 @@
 import { CommentsService } from './comments.service';
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { AddCommentsDto } from './dto/comment.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+
 import { AuthGuard } from '@nestjs/passport';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
+import { AddCommentsDto } from './dto';
 
-@UseGuards(JwtGuard) //==> controller level guard
+//==> controller level guard
 @Controller('comments')
 export class CommentsController {
   constructor(private CommentsService: CommentsService) {}
   // @UseGuards(AuthGuard('jwt')) // we can simply write this but we seperate  class for guarde
   // @UseGuards(JwtGuard) ==> guard at router level is for perticuler route so we transform that guarde to controller level to appy for app routes
-  @Post('/addcomment')
+  @UseGuards(JwtGuard)
+  @Post('addcomment')
   // addcomments(@GetUser() dto: AddCommentsDto) {// if we have to add custome decorator  then
   // addcomments(@GetUser() user:User , @GetUser('email') email:string) { ==> in this we can get perticuler data from the user
-  addcomments(@Req() Req: Request) {
-    return this.CommentsService.addcomment(Req);
+  addcomments(@Body() dto: AddCommentsDto, @GetUser('id') id: string) {
+    return this.CommentsService.addcomment(dto, id);
+  }
+
+  @Get('getcommentofvideo/:id')
+  getcommetnsvideo(@Param('id') videoid: string) {
+    return this.CommentsService.getcommetnsvideo(videoid);
   }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,7 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AddCommentsDto } from './dto/comment.dto';
 
 @Injectable()
 export class CommentsService {
-  async addcomment(Req) {}
+  constructor(private prisma: PrismaService) {}
+  async addcomment(dto: AddCommentsDto, id) {
+    try {
+      const comment = await this.prisma.comments.create({
+        data: {
+          message: dto.comment,
+          User: {
+            connect: {
+              id: id,
+            },
+          },
+          video: {
+            connect: {
+              id: dto.Video_id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      return 'ok';
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getcommetnsvideo(videoid: string) {
+    try {
+      const comment = await this.prisma.video.findUnique({
+        where: {
+          id: videoid,
+        },
+        select: {
+          Comments: {
+            select: {
+              message: true,
+              User: {
+                select: {
+                  picture: true,
+                  name: true,
+                  id: true,
+                },
+              },
+            },
+          },
+
+          createdAt: true,
+        },
+      });
+      return comment;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
