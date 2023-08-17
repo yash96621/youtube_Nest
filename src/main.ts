591d9a0eb799fe2b3cebe21b62110efc2cdf9606
yash96621 <101980618+yash96621@users.noreply.github.com>
2023-08-17 14:31:34 +0530

    Update main.ts
    u0date cors error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 // }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //to another paramater can not accept
