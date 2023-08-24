022fb188e032491a927d6595653a63fe7ccad91b
Yash Savani <yashsavani@Yashs-MacBook-Pro.local>
2023-08-24 09:20:04 +0530

    main ts 4 gb done

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from '@nestjs/common';
+// import { urlencoded, json } from 'express';
+import * as bodyParser from 'body-parser';
 
 // function globalmiddleware(req:Request ,res:Response , next:NextFunction){
 //   console.log('this is global middleware');
@@ -9,9 +11,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.enableCors({
-    origin: ['http://65.0.68.166'],
+  app.enableCors({
+    origin: [
+      'http://65.0.68.166',
+      'http://localhost:5000',
+      'http://localhost:3000',
+    ],
   });
+  // app.use(json({ limit: '4gb' }));
+  // app.use(urlencoded({ extended: true, limit: '4gb' }));
+
+  app.use(bodyParser.json({ limit: '4gb' }));
+  app.use(bodyParser.urlencoded({ limit: '4gb', extended: true }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //to another paramater can not accept
